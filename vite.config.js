815607cf927102c386a97a35d2a897a44c734e42
DanoR <zheung@vip.qq.com>
2021-07-22 16:22:42 +0800

    update package enable `vite/legacy`

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite';
 import pluginVue from '@vitejs/plugin-vue';
+import pluginLegacy from '@vitejs/plugin-legacy';
 
 export default defineConfig({
-	plugins: [pluginVue()],
+	plugins: [
+		pluginVue(),
+		pluginLegacy({ targets: ['defaults', 'not IE 11'] }),
+	],
 	base: './',
 	build: {
 		emptyOutDir: false,
 		outDir: '../lol-colorgrid-prod',
+		target: 'es2015'
 	}
 });
